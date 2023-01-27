63fddd662d57e9d4cd1dcd0f090504c9ff5e45fa
Red <red.pace.dev@gmail.com>
2023-01-27 12:51:11 +0100

    chore: Combined register page with home page

--- react/player/components/PlayerRegisterButton.tsx
+++ react/player/components/PlayerRegisterButton.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 import * as React from "react";
 import { useAccount } from "wagmi";
 
-import { useIsRegistered } from "@/react/api";
+import { useIsRegistered, useRegister } from "@/react/api";
 
 export const PlayerRegisterButton = React.memo(
   function PlayerRegisterButton(): JSX.Element {
@@ -12,15 +12,56 @@ export const PlayerRegisterButton = React.memo(
       address,
     });
 
+    console.log("loadingIsRegistered", loadingIsRegistered);
+    console.log("isRegistered", isRegistered);
+
     return (
-      <Link
-        href="/register"
-        style={{
-          color: loadingIsRegistered ? "grey" : isRegistered ? "red" : "green",
-        }}
-      >
-        Tap here to register.
-      </Link>
+      <div className="w-full flex flex-col h-full">
+        <h1 className="text-4xl my-4 lg:my-6 text-center">
+          Register as a player 😎
+        </h1>
+        <Form />
+      </div>
     );
   }
 );
+
+const Form = () => {
+  const { isConnected } = useAccount();
+  const router = useRouter();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { register } = useRegister();
+
+  const onPressRegister = React.useCallback(async () => {
+    try {
+      await register({});
+      await router.push("/");
+    } catch (e) {
+      console.error(e);
+    }
+  }, [register, router]);
+
+  console.log(isConnected);
+
+  if (!isConnected) {
+    return (
+      <div className="flex flex-wrap flex-col content-center text-center">
+        <p className="mb-2">Connect a wallet first!</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-md mx-auto">
+      <p className="flex justify-center">
+        <button
+          className="bg-black px-4 py-2 rounded text-white"
+          onClick={onPressRegister}
+        >
+          Register
+        </button>
+      </p>
+    </div>
+  );
+};
