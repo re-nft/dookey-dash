12d0831b9e2eb05ad07d2e561fd1867584b3b43f
cawfree <hello@cawfree.com>
2023-01-29 09:37:22 +0000

    fix: layout

--- react/modals/components/Modal.DelegateTo.tsx
+++ react/modals/components/Modal.DelegateTo.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import * as React from "react";
 import Modal, { useModalState } from "react-simple-modal-provider";
 
-import {useDelegatedAddresses} from "@/react/api";
+import { useDelegatedAddresses } from "@/react/api";
 import { useBaseModalProps } from "@/react/modals";
 import { ID_MODAL_DELEGATE_TO } from "@/react/modals/consts";
 
@@ -12,20 +12,31 @@ export function ModalDelegateTo({
   children,
 }: React.PropsWithChildren): JSX.Element {
   const modalState = useModalState();
-  const [, setModalState] = modalState;
+  const [isOpen, setModalState] = modalState;
   const router = useRouter();
+
+  const wasOpen = React.useRef<boolean>(false);
+
+  React.useEffect(() => {
+    const { current: wasJustOpen } = wasOpen;
+    wasOpen.current = isOpen;
+
+    if (!isOpen && wasJustOpen) router?.replace("/");
+  }, [isOpen]);
 
   const onBeforeDelegateToken = React.useCallback(() => {
     setModalState(false);
-    router?.replace("/");
   }, [router]);
 
   // HACK: Since we use react-query, we can ask for delegatedAddresses
   //       to be refreshed here and incur an update for the rest of the
   //       application.
-  const {refetch: refetchDelegatedAddresses} = useDelegatedAddresses();
+  const { refetch: refetchDelegatedAddresses } = useDelegatedAddresses();
 
-  const onAfterDelegateToken = React.useCallback(() => refetchDelegatedAddresses(), [refetchDelegatedAddresses]);
+  const onAfterDelegateToken = React.useCallback(
+    () => refetchDelegatedAddresses(),
+    [refetchDelegatedAddresses]
+  );
 
   return (
     <Modal
