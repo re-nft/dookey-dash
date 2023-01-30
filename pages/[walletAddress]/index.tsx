8c7a90e4c715097c4f2acc3edf9bf1e2e8419f39
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2023-01-30 16:52:38 +0100

    feat: show 'no sewer passes owned'

--- pages/[walletAddress]/index.tsx
+++ pages/[walletAddress]/index.tsx
@@ -1,22 +1,32 @@
 import { OwnedNft } from "alchemy-sdk";
 import { ethers } from "ethers";
 import { useRouter } from "next/router";
-import {toWords} from "number-to-words";
+import { toWords } from "number-to-words";
 import * as React from "react";
-import {isDelegateCashResult, useDelegateCash, useGetTokenLevelDelegations} from "use-delegatecash";
+import {
+  isDelegateCashResult,
+  useDelegateCash,
+  useGetTokenLevelDelegations,
+} from "use-delegatecash";
 import { useAccount } from "wagmi";
 
 import { compareAddresses } from "@/common/address.utils";
 import { CONTRACT_ADDRESS_SEWER_PASS } from "@/config";
 import { useDelegatedAddresses, useSewerPasses } from "@/react/api";
-import {useAllowModal, useRevokeAllModal, useRevokeModal} from "@/react/modals";
+import {
+  useAllowModal,
+  useRevokeAllModal,
+  useRevokeModal,
+} from "@/react/modals";
 
 const getMaybeDelegationForOwnedNft = ({
   ownedNft,
   tokenLevelDelegations: maybeTokenLevelDelegations,
 }: {
   readonly ownedNft: OwnedNft;
-  readonly tokenLevelDelegations: ReturnType<typeof useGetTokenLevelDelegations>;
+  readonly tokenLevelDelegations: ReturnType<
+    typeof useGetTokenLevelDelegations
+  >;
 }) => {
   if (!isDelegateCashResult(maybeTokenLevelDelegations)) return null;
 
@@ -44,41 +54,58 @@ const getTierForToken = (ownedNft: OwnedNft): number => {
 
 function WalletAddressPageForCurrentUser(): JSX.Element {
   const delegateCash = useDelegateCash();
-  const {addresses, refetch} = useDelegatedAddresses();
+  const { addresses, refetch } = useDelegatedAddresses();
 
-  const {open: openRevokeModal} = useRevokeModal();
-  const {open: openRevokeAllModal} = useRevokeAllModal();
+  const { open: openRevokeModal } = useRevokeModal();
+  const { open: openRevokeAllModal } = useRevokeAllModal();
 
-  const onClickRevokeAll = React.useCallback(
-    async () => {
+  const { address: connectedAddress } = useAccount();
+
+  const { data: maybeSewerPasses } = useSewerPasses({
+    address: connectedAddress,
+  });
+
+  const onClickRevokeAll = React.useCallback(async () => {
+    try {
+      await delegateCash.revokeAllDelegates();
+      openRevokeAllModal({});
+      await refetch();
+    } catch (e) {
+      console.error(e);
+    }
+  }, [delegateCash, openRevokeAllModal, refetch]);
+
+  const onClickRevokeDelegate = React.useCallback(
+    async (delegate: string) => {
       try {
-        await delegateCash.revokeAllDelegates();
-        openRevokeAllModal({});
+        await delegateCash.revokeDelegate(delegate);
+        openRevokeModal({ nameOfRevokedToken: "Sewer Pass" });
         await refetch();
       } catch (e) {
         console.error(e);
       }
     },
-    [delegateCash, openRevokeAllModal, refetch]
+    [delegateCash, openRevokeModal, refetch]
   );
-
-  const onClickRevokeDelegate = React.useCallback(async (delegate: string) => {
-    try {
-      await delegateCash.revokeDelegate(delegate);
-      openRevokeModal({ nameOfRevokedToken: "Sewer Pass" });
-      await refetch();
-    } catch (e) {
-      console.error(e);
-    }
-  }, [delegateCash, openRevokeModal, refetch]);
 
   return (
     <div>
-      <span children={`You've delegated to ${toWords(addresses.length)} address${addresses.length === 1 ? "" : "es"}.`} />
+      {(maybeSewerPasses || []).length > 0 ? (
+        <span
+          children={`You've delegated to ${toWords(addresses.length)} address${
+            addresses.length === 1 ? "" : "es"
+          }.`}
+        />
+      ) : (
+        <span children="You don't own any Sewer Passes." />
+      )}
       {addresses.length > 1 ? (
         <button
           onClick={onClickRevokeAll}
-          className={"p-5 w-full m-3 bg-[#A855F7] shadow-md rounded text-white uppercase md:w-auto md:px-4 md:py-2"}>
+          className={
+            "p-5 w-full m-3 bg-[#A855F7] shadow-md rounded text-white uppercase md:w-auto md:px-4 md:py-2"
+          }
+        >
           Revoke All Delegates
         </button>
       ) : (
@@ -138,27 +165,33 @@ function WalletAddressPageForAnotherUser({
         console.error("Failed to delegate token.", { cause });
       }
     },
-    [refetchDelegatedAddresses, delegateCash, addressToDelegateTo, openAllowModal]
+    [
+      refetchDelegatedAddresses,
+      delegateCash,
+      addressToDelegateTo,
+      openAllowModal,
+    ]
   );
 
   // Only render tokens that haven't already been delegated.
-  const data: OwnedNft[] = (maybeData || []);
+  const data: OwnedNft[] = maybeData || [];
 
   const didDelegateToUser = React.useMemo(
-    () => data.find((ownedNft: OwnedNft) => {
-      const maybeDelegation = getMaybeDelegationForOwnedNft({
-        ownedNft,
-        tokenLevelDelegations: maybeTokenLevelDelegations,
-      });
+    () =>
+      data.find((ownedNft: OwnedNft) => {
+        const maybeDelegation = getMaybeDelegationForOwnedNft({
+          ownedNft,
+          tokenLevelDelegations: maybeTokenLevelDelegations,
+        });
 
-      if (!maybeDelegation) return false;
+        if (!maybeDelegation) return false;
 
-      return compareAddresses(maybeDelegation.delegate, addressToDelegateTo);
-    }),
+        return compareAddresses(maybeDelegation.delegate, addressToDelegateTo);
+      }),
     [data, addressToDelegateTo, maybeTokenLevelDelegations]
   );
 
-  const {open: openRevokeModal} = useRevokeModal();
+  const { open: openRevokeModal } = useRevokeModal();
 
   const onClickRevoke = React.useCallback(async () => {
     try {
@@ -168,27 +201,37 @@ function WalletAddressPageForAnotherUser({
     } catch (e) {
       console.error(e);
     }
-  }, [delegateCash, addressToDelegateTo, openRevokeModal, refetchDelegatedAddresses]);
+  }, [
+    delegateCash,
+    addressToDelegateTo,
+    openRevokeModal,
+    refetchDelegatedAddresses,
+  ]);
 
   return (
-      <div style={{ overflow: "scroll" }}>
-        {Boolean(didDelegateToUser) && (
-          <div>
-            <button
-              onClick={onClickRevoke}
-              className={"p-5 w-full m-3 bg-[#A855F7] shadow-md rounded text-white uppercase md:w-auto md:px-4 md:py-2"}>
-              Revoke Access
-            </button>
-          </div>
-        )}
-        {[1, 2, 3, 4]
-        .map((tier: number) => data.filter((ownedNft) => getTierForToken(ownedNft) === tier))
+    <div style={{ overflow: "scroll" }}>
+      {Boolean(didDelegateToUser) && (
+        <div>
+          <button
+            onClick={onClickRevoke}
+            className={
+              "p-5 w-full m-3 bg-[#A855F7] shadow-md rounded text-white uppercase md:w-auto md:px-4 md:py-2"
+            }
+          >
+            Revoke Access
+          </button>
+        </div>
+      )}
+      {[1, 2, 3, 4]
+        .map((tier: number) =>
+          data.filter((ownedNft) => getTierForToken(ownedNft) === tier)
+        )
         .flatMap((tokensForTier) => {
           const sortedTokens = tokensForTier.sort(
             (a, b) => Number(a.tokenId) - Number(b.tokenId)
           );
           return sortedTokens.map((ownedNft: OwnedNft) => {
-            const {tokenId} = ownedNft;
+            const { tokenId } = ownedNft;
             const tier = getTierForToken(ownedNft);
 
             const maybeDelegatedToken = getMaybeDelegationForOwnedNft({
@@ -214,7 +257,7 @@ function WalletAddressPageForAnotherUser({
             );
           });
         })}
-      </div>
+    </div>
   );
 }
 
@@ -222,7 +265,11 @@ export default React.memo(function WalletAddressPage(): JSX.Element {
   const { address } = useAccount();
   const { walletAddress } = useRouter().query;
 
-  if (typeof walletAddress !== "string" || !ethers.utils.isAddress(walletAddress)) return <></>;
+  if (
+    typeof walletAddress !== "string" ||
+    !ethers.utils.isAddress(walletAddress)
+  )
+    return <></>;
 
   if (compareAddresses(address, walletAddress))
     return <WalletAddressPageForCurrentUser />;
