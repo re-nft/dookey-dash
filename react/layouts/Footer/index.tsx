8fd15cedf95fab25d3d8df23b42a956492f23e25
cawfree <hello@cawfree.com>
2023-01-30 10:59:18 +0000

    chore: add revoke token

--- react/layouts/Footer/index.tsx
+++ react/layouts/Footer/index.tsx
@@ -7,29 +7,36 @@ export const Footer = () => {
   return (
     <footer className="p-4 lg:p-8 flex flex-row flex-nowrap">
       <div className="flex-auto grow flex flex-col">
-        <span className="text-[#000000] text-[12px] md:text-base">
-          Powered By
-        </span>
-        <a href="https://renft.io">
-          <Image
-            className="inline-block w-32 text-[#000000] md:w-60"
-            width="100"
-            height="100"
-            src="/renft-logo-black-and-white.svg"
-            fallbackSrc="/renft-logo-black-and-white.svg"
-            alt="reNFT"
-          />
-        </a>
+        {false && (
+          <>
+            <span className="text-[#000000] text-[12px] md:text-base">
+              Powered By
+            </span>
+            <a href="https://renft.io">
+              <Image
+                className="inline-block w-32 text-[#000000] md:w-60"
+                width="100"
+                height="100"
+                src="/renft-logo-black-and-white.svg"
+                fallbackSrc="/renft-logo-black-and-white.svg"
+                alt="reNFT"
+              />
+            </a>
+          </>
+        )}
       </div>
       <div className="flex-auto grow-0 self-center">
-        <a className={`${footerLinks}`} href="/">
-          Discord
+        <a className={`${footerLinks} mr-0`} href="https://delegate.cash">
+          delegate.cash
         </a>
-        <a className={`${footerLinks}`} href="/">
-          Twitter
+        <a
+          className={`${footerLinks}`}
+          href="https://discord.com/invite/4Ab8tknmhf"
+        >
+          discord
         </a>
-        <a className={`${footerLinks} mr-0`} href="/">
-          Telegram
+        <a className={`${footerLinks}`} href="https://twitter.com/renftlabs">
+          twitter
         </a>
       </div>
     </footer>
