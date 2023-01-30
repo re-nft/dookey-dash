d8db5ec1443221088d1ea9fb4ac6c45730f1f6fc
cawfree <hello@cawfree.com>
2023-01-30 16:39:12 +0000

    fix: friendlier message signing

--- common/signature.utils.ts
+++ common/signature.utils.ts
@@ -1,9 +1,15 @@
 import { ethers } from "ethers";
 
 // TODO: rename to createMessageToSign
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const rawMessageToSignedMessage = (rawMessage: string) => {
-  const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(rawMessage));
-  return `\x19Ethereum Signed Message:\n${32}\n${hash}`;
+  //const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(rawMessage));
+  //return `\x19Ethereum Signed Message:\n${32}\n${hash}`;
+  return `
+Hey, thanks for using dookey.renft.io! We appreciate you!
+
+Please sign this message to request a Sewer Pass to be delegated to you.
+  `.trim();
 };
 
 export const verifySignature = ({
