951bda47907aabf0eaf23978a91b3e845ade2f1c
cawfree <hello@cawfree.com>
2023-01-31 11:59:56 +0000

    chore: bauti fixes

--- common/random.utils.tsx
+++ common/random.utils.tsx
@@ -1,4 +1,4 @@
-import {create} from "random-seed";
+import { create } from "random-seed";
 import * as React from "react";
 
 type WaitingString = {
@@ -7,23 +7,91 @@ type WaitingString = {
 };
 
 const WAITING_STRINGS: readonly WaitingString[] = [
-  {prefix: "", suffix: " nearly got drained on the way over here." },
-  {prefix: "", suffix: " may look dumb, but... No wait, that's everything." },
-  {prefix: "", suffix: " seems like a good bet. Smells like a sewer." },
-  {prefix: "", suffix: " got tired of paper-handed flipping ape derivatives."},
-  {prefix: "", suffix: " quit their day job to flip NFTs. How is that going by the way?"},
-  {prefix: "", suffix: " just finished their shift at McDonalds."},
-  {prefix: "If unicode had stink lines, we'd be drawing them on ", suffix: "."},
-  {prefix: "", suffix: ' told their boss they were conducting "fintech research" just to be here.'},
-  {prefix: "", suffix: " still dabbles in food tokens."},
-  {prefix: "", suffix: " is fresh from the sewers."},
-  {prefix: "", suffix: ", JPEG connoisseur. Ooh la la!"},
-  {prefix: "", suffix: " (not financial advice)"},
-  {prefix: "", suffix: " firmly believes TA is astrology for degens."},
-  {prefix: "", suffix: " still thinks there's a future in BitConnect."},
-  {prefix: "", suffix: " doesn't have game. Literally."},
-  {prefix: "", suffix: " a.k.a  Queen of the Rats"},
-  {prefix: "", suffix: ", renowned rug pull survivor. "},
+  { prefix: "", suffix: " may look dumb, but... No wait, that's everything." },
+  { prefix: "", suffix: " seems like a good bet. Smells like a sewer." },
+  {
+    prefix: "",
+    suffix: " got tired of paper-handed flipping ape derivatives.",
+  },
+  {
+    prefix: "",
+    suffix: " quit their day job to flip NFTs. How is that going by the way?",
+  },
+  { prefix: "", suffix: " just finished their shift at McDonalds." },
+  {
+    prefix: "If unicode had stink lines, we'd be drawing them on ",
+    suffix: ".",
+  },
+  {
+    prefix: "",
+    suffix:
+      ' told their boss they were conducting "fintech research" just to be here.',
+  },
+  { prefix: "", suffix: " still dabbles in food tokens." },
+  { prefix: "", suffix: " is fresh from the sewers." },
+  { prefix: "", suffix: ", JPEG connoisseur. Ooh la la!" },
+  { prefix: "", suffix: " (not financial advice)" },
+  { prefix: "", suffix: " firmly believes TA is astrology for degens." },
+  { prefix: "", suffix: " still thinks there's a future in BitConnect." },
+  { prefix: "", suffix: " doesn't have game. Literally." },
+  { prefix: "", suffix: ", renowned rug pull survivor. " },
+  { prefix: "", suffix: " dreams of a decentralized world." },
+  { prefix: "", suffix: " got lost navigating the crypto world." },
+  { prefix: "", suffix: " is all about yield farming, 24/7." },
+  { prefix: "", suffix: " is waiting for the next bull market." },
+  { prefix: "", suffix: " just took their first L." },
+  { prefix: "", suffix: " thinks NFTs are the new potatoes." },
+  {
+    prefix: "",
+    suffix: " still has trust issues from the great Ethereum crash of 2021.",
+  },
+  {
+    prefix: "",
+    suffix: " is considering a career change to a full-time meme trader.",
+  },
+  {
+    prefix: "",
+    suffix: " is pretty sure they just bought a piece of the moon.",
+  },
+  {
+    prefix: "",
+    suffix: " is hoping to one day retire on their Mekaverse collection.",
+  },
+  { prefix: "", suffix: " thinks the real gem in NFTs is the memes." },
+  { prefix: "", suffix: ", collector of all things absurd." },
+  { prefix: "", suffix: " (but only for the lols)" },
+  {
+    prefix: "",
+    suffix: " firmly believes that HODL is a valid life strategy.",
+  },
+  { prefix: "", suffix: " a.k.a King of the Meme Trading Game" },
+  { prefix: "", suffix: " thinks NFTs are like Pokemon, gotta catch 'em all." },
+  {
+    prefix: "",
+    suffix:
+      " still believes that their Bored Ape is worth more than their car.",
+  },
+  {
+    prefix: "",
+    suffix:
+      " is convinced that the future of art is just memes on the blockchain.",
+  },
+  {
+    prefix: "",
+    suffix: " is pretty sure they just bought a piece of the sun, too.",
+  },
+  {
+    prefix: "",
+    suffix: " is hoping to retire on a beach made entirely of NFTs.",
+  },
+  {
+    prefix: "",
+    suffix: " still hasn't recovered from their CryptoKitties addiction.",
+  },
+  { prefix: "", suffix: " believes that memes are the new gold standard." },
+  { prefix: "", suffix: " (for the lols and maybe some profits)" },
+  { prefix: "", suffix: " firmly believes that HODL is the new YOLO." },
+  { prefix: "", suffix: " a.k.a the Meme Trading Guru" },
 ];
 
 export const getRandomWaitingString = (address: string): WaitingString => {
--- react/components/Cover/index.tsx
+++ react/components/Cover/index.tsx
@@ -11,10 +11,12 @@ export const Cover = ({ children }: { children?: ReactNode }) => (
     </h1>
 
     <p className={`text-xl max-w-sm text-white mx-auto my-10 ${c.poop}`}>
-      Request to play Dookey Dash from existing holders, or allow others to play with your pass to increase your odds of winning.
+      Request to play Dookey Dash from existing holders, or allow others to play
+      with your pass to increase your odds of winning.
     </p>
-    <p className={`text-xl max-w-sm text-white mx-auto my-10 ${c.poop}`}>
-        Made possible by <a href="https://delegate.cash">delegate.cash</a>!
+    <p className={`text-xl max-w text-white mx-auto my-10 ${c.poop}`}>
+      Made possible by <a href="https://delegate.cash">delegate.cash</a> and{" "}
+      <a href="https://renft.io">renftlabs</a>!
     </p>
 
     <p className="flex-auto grow-0 flex flex-row flex-wrap justify-center">
