b2d46cab4f635ccf4b1b340a7acd860c032aa2bb
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-01-31 15:49:38 +0100

    Poop main navigation (and links)

--- react/layouts/components/nav.tsx
+++ react/layouts/components/nav.tsx
@@ -1,16 +1,17 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import React from "react";
-import {useAccount} from "wagmi";
+import { useAccount } from "wagmi";
 
-const navLinks =
-  "mx-1 text-[#A855F7] whitespace-nowrap md:text-base md:mx-4";
+import c from "./nav.module.css";
+
+const navLinks = `text-lg p-3 text-dookey-green whitespace-nowrap md:mx-4 ${c.poop}`;
 
 export const Nav = ({ className = "" }: { className?: string }) => {
-  const {isConnected, address} = useAccount();
+  const { isConnected, address } = useAccount();
   return (
     <nav
-      className={`w-full flex flex-row justify-between items-center p-4 ${className}`}
+      className={`w-full flex flex-row flex-wrap items-center justify-center gap-4 p-4 ${className}`}
     >
       <div className="flex flex-row flex-nowrap gap-4 items-center">
         <Link href="/" className={`${navLinks} font-semibold`}>
