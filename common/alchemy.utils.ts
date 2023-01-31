b0fd343392dccb014f4e8e163c9673cc601f7b25
cawfree <hello@cawfree.com>
2023-01-31 09:40:58 +0000

    fix: alchemy

---
+++ common/alchemy.utils.ts
@@ -0,0 +1,20 @@
+import {env} from "@/server/env";
+import {ethers} from "ethers";
+
+export const getAlchemyApiKey = () => {
+  if (env.NEXT_PUBLIC_ALCHEMY_API_KEY) return env.NEXT_PUBLIC_ALCHEMY_API_KEY;
+  if (env.ALCHEMY_API_KEY) return env.ALCHEMY_API_KEY;
+
+  // https://github.com/cawfree/react-native-helios/blob/64029d149eda3c6cb329ab129f64939b03cb6ac7/example/src/App.tsx#L15
+  return 'pPwfAKdQqDr1OP-z5Txzmlk0YE1UvAQT';
+};
+
+export const API_ALCHEMY_PROVIDER = new ethers.providers.AlchemyProvider(
+  "mainnet",
+  getAlchemyApiKey(),
+);
+
+const signer = ethers.Wallet.createRandom();
+
+// HACK: Force delegateCash to instantiate with an incompatible provider.
+Object.assign(API_ALCHEMY_PROVIDER, { getSigner: () => signer });
